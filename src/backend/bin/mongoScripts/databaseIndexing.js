24e28d75b2375b03d55f8450da848705cce92550
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2014-01-13 19:12:33 +0700

    Update doc, upload image in facility

--- src/backend/bin/mongoScripts/databaseIndexing.js
+++ src/backend/bin/mongoScripts/databaseIndexing.js
@@ -1,3 +1,3 @@
-db.facilities.ensureIndex("facilityName" : 1, "city" : 1, "state" : 1);
-db.classes.ensureIndex("className" : 1, "facilityName" : 1, "classDescription" : 1);
- : regex} , {  : regex}, { schedule : {$elemMatch: {dayOfWeek: search.toLowerCase()}} }, { classDescription : regex}] }
+db.facilities.ensureIndex({"facilityName" : 1, "city" : 1, "state" : 1});
+db.classes.ensureIndex({"className" : 1, "facilityName" : 1, "schedule" : 1, "classDescription" : 1, "price" : 1});
+db.events.ensureIndex({"eventName" : 1, "address1" : 1, "address2" : 1, "city" : 1, "stateProvinceCode" : 1, "country" : 1, "startDateTime" : 1, "endDateTime" : 1, "eventPrice" : 1}, {name:"eventIndex"});
